09942f734af08d14bc2ddce14c338992548f4781
Carlos Roberto <carlos.reis.33@hotmail.com>
2023-03-20 20:34:58 -0300

    Selectors in test

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -3,15 +3,21 @@ import { Button, Text, TextInput, View } from "react-native";
 export function Profile() {
   return (
     <View>
-      <Text>Perfil</Text>
+      <Text testID="text-title">
+        Perfil
+      </Text>
 
       <TextInput 
+        testID="input-name"
         placeholder="Nome"
         autoCorrect={false}
+        value="Carlos"
       />
 
       <TextInput 
+        testID="input-surname"
         placeholder="Sobrenome"
+        value="Roberto"
       />
 
       <Button 
