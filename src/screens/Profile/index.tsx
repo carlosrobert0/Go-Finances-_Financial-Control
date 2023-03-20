a3fb6c0b4816829623a39dad1ce6ae998886a419
Carlos Roberto <carlos.reis.33@hotmail.com>
2023-03-20 15:28:49 -0300

    Render in tests

---
+++ src/screens/Profile/index.tsx
@@ -0,0 +1,23 @@
+import { Button, Text, TextInput, View } from "react-native";
+
+export function Profile() {
+  return (
+    <View>
+      <Text>Perfil</Text>
+
+      <TextInput 
+        placeholder="Nome"
+        autoCorrect={false}
+      />
+
+      <TextInput 
+        placeholder="Sobrenome"
+      />
+
+      <Button 
+        title="Salvar"
+        onPress={() => {}}
+      />
+    </View>
+  )
+}
