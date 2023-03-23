7f55025c7c6c2d1894c7867fda0de7e7f6fdcde0
croberto <croberto@quanticosolutions.com>
2023-03-22 22:22:27 -0300

    Using mocks

--- src/hooks/auth.spec.tsx
+++ src/hooks/auth.spec.tsx
@@ -1,8 +1,34 @@
 import { act, renderHook } from '@testing-library/react-native'
 import { AuthProvider, useAuth } from './auth'
 
+jest.mock('expo-auth-session', () => {
+  return {
+    startAsync: () => {
+      return {
+        type: 'success',
+        params: {
+          access_token: 'google-token'
+        }
+      }
+    }
+  }
+})
+
+
+
 describe('Auth Hook', () => {
   it('should be able to sign in with Google account existing', async () => {
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve({
+        id: `userInfo.id`,
+        email: `userInfo.email`,
+        name: `userInfo.given_name`,
+        photo: `userInfo.picture`,
+        locale: `userInfo.locale`,
+        verified_email: `userInfo.verified_email`
+      })
+    })) as jest.Mock
+    
     const { result } = renderHook(() => useAuth(), {
       wrapper: AuthProvider
     })
